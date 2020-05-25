634124bfa2892e3815a98d25bf81387f073e2463
Veiler <linhnv110196@gmail.com>
2020-05-25 15:59:49 +0700

    First Screen

--- App.js
+++ App.js
@@ -1,2 +1,21 @@
-import React from 'react';
-import { Text , View, Image } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, StatusBar, View, Platform, Image, Text, Colors } from "react-native";
+import BGP from "./Picture/backgroundpic.jpg";
+
+const STATUS_BAR_HEIGHT = Platform.OS === "ios" ? 20 : StatusBar.currentHeight;
+
+class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar barStyle="light-content" />
+        <Image source={BGP}
+        style={{
+            flex:0.192
+        }}/>
+      </View>
+    );
+  }
+}
+Lungta
+export default App;
