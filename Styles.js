ccd2821ba3e6a10f325b9bacaf68760ab6db69fc
quandepzai <43404519+quandepzai@users.noreply.github.com>
2020-05-25 15:46:07 +0700

    style

---
+++ Styles.js
@@ -0,0 +1,7 @@
+export const colors = {
+  greyishBrown: 'rgb(71, 71, 71)',
+  white: 'rgb(224, 224, 224)',
+  velvet: 'rgb(114, 13, 93)',
+  blueyGrey: 'rgb(150, 158, 178)',
+  paleGreyFour: 'rgb(230, 236, 240)'
+}
