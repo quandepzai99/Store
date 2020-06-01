e6b743c6dc080e34dfc6f61c0fa7e72b5035431e
quandepzai <43404519+quandepzai@users.noreply.github.com>
2020-06-01 17:16:29 +0700

    Final

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import {createStackNavigator} from "@react-navigation/stack";
 import LoginScreen from './screens/LoginScreen';
 import PasswordScreen from './screens/PasswordScreen';
 import { navigationRef } from './screens/RootNavigation';
+import ChangePasswordsScreen from './screens/ChangePasswordScreen';
+import ChangePasswordsScreenStep1 from './screens/ChangePasswordScreenStep1';
+
 
 
 // const navigationRef = React.createRef();
@@ -15,8 +18,10 @@ export default class App extends Component {
     return (
       <NavigationContainer ref={navigationRef}>
         <Stack.Navigator headerMode="none">
-          <Stack.Screen name={'Telephone'} component={LoginScreen} />
-          <Stack.Screen name={'PinCode'} component={PasswordScreen} />
+          {/*<Stack.Screen name={'Telephone'} component={LoginScreen} />*/}
+          {/*<Stack.Screen name={'PinCode'} component={PasswordScreen} />*/}
+          {/*<Stack.Screen name={'PinCode'} component={ChangePasswordsScreen} />*/}
+          <Stack.Screen name={'PinCode'} component={ChangePasswordsScreenStep1} />
         </Stack.Navigator>
       </NavigationContainer>
     );
