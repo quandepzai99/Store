fdb130eac41d68eceb506c2494443f622ee4e26c
Veiler <linhnv110196@gmail.com>
2020-06-05 15:41:28 +0700

    add button nap the cho Header
    Signed-off-by: Veiler <linhnv110196@gmail.com>

--- Common/images.js
+++ Common/images.js
@@ -3,7 +3,8 @@ const images = {
   chat: require('../Picture/chat.png'),
   sigleout: require('../Picture/signout.png'),
   witness: require('../Picture/witness.png'),
-  cross : require('../Picture/icon-Cross/bt.png')
+  cross : require('../Picture/icon-Cross/bt.png'),
+  banner : require('../Picture/Banner/banner.jpg')
 }
 
 export default images;
