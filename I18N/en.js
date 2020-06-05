4d7e8994d415517bec901615953e8a31b8b3feb9
Veiler <linhnv110196@gmail.com>
2020-06-05 09:03:26 +0700

    add duo language for Password Screen

--- I18N/en.js
+++ I18N/en.js
@@ -1,5 +1,8 @@
 export default {
   AuthenticationScreenLoginText: "Welcome to UrBox",
   AuthenticationScreenPhoneText: "Enter you phone number",
-  AuthenticationScreenEnterPhoneNumberText: "Phone number"
+  AuthenticationScreenEnterPhoneNumberText: "Phone number",
+  EnterPasswordScreenEnterPasswordText : "Input Your Password",
+  EnterPasswordScreenForgotPasswordText : "Forgot Your Password?",
+  EnterPasswordScreenSignOutText : "Sign Out"
 };
