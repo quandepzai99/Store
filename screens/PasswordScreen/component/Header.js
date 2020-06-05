007d001b8a07c50debf51590b6eda4fc68cfc56e
Veiler <linhnv110196@gmail.com>
2020-06-05 09:42:41 +0700

    final flow build
    Signed-off-by: Veiler <linhnv110196@gmail.com>

--- screens/PasswordScreen/component/Header.js
+++ screens/PasswordScreen/component/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
   render() {
     return (
       <ImageBackground source={images.HeaderBackground} style={{ height: 224 }}>
-        <Text style={styles.text}> Chào mừng quý khách đến với UrBox</Text>
+        <Text style={styles.text}> Chào mừng bạn quay lại UrBox</Text>
       </ImageBackground>
     );
   }
