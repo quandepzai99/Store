482487a4c38aaf1f8ae7ba77e0c4bc160f6fc81b
quandepzai <43404519+quandepzai@users.noreply.github.com>
2020-06-05 15:26:07 +0700

    Flatlist

--- HomePage-Flow/HomeScreen/Components/SlideBanner.js
+++ HomePage-Flow/HomeScreen/Components/SlideBanner.js
@@ -1,18 +1,59 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, Text} from "react-native";
+import {View, StyleSheet, Text, FlatList, TouchableOpacity, SafeAreaView, Image} from "react-native";
 import {colors} from "../../../Styles";
-import AntDesign from 'react-native-vector-icons/AntDesign'
+import images from "../../../Common/images";
 
+const tempData = [
+  {
+    id: 1,
+    image: '',
+    name: 'quan'
+  },
+  {
+    id: 2,
+    image: '',
+    name: 'quan'
+  },
+  {
+    id: 3,
+    image: '',
+    name: 'quan'
+  },
+]
 export default class SlideBanner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+ _renderItem = ({item, index}) => {
+
+    return(
+      <View style={styles.btnBanner}>
+        {/*<Image source={{uri: item.image}} />*/}
+        <Text>{item.name}</Text>
+      </View>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.category}>
-
-        </View>
         <Text style={styles.title}>
-          Tin tức <Text style={{color: 'gold', fontWeight: 'bold', fontSize: 24}}>nổi bật</Text>
+          Tin tức <Text style={{color: colors.tangerine, fontWeight: 'bold', fontSize: 24}}>nổi bật</Text>
         </Text>
+        <View style={{padding: 32, height: 248}}>
+          <FlatList
+            data={tempData}
+            renderItem={this._renderItem}
+            horizontal={true}
+            showHorizontalScrollIndicator={false}
+            keyExtractor={item => item.name}
+
+          />
+        </View>
       </View>
     );
   }
@@ -24,13 +65,13 @@ const styles = StyleSheet.create({
     top: 33
   },
   title: {
+    marginLeft: 16,
     fontFamily: "Roboto",
     fontSize: 24,
     fontWeight: "normal",
     fontStyle: "normal",
     lineHeight: 26,
     letterSpacing: 0,
-    textAlign: "left",
     color: colors.velvet
   },
   category: {
--- HomePage-Flow/HomeScreen/index.js
+++ HomePage-Flow/HomeScreen/index.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react';
-import {View, StatusBar} from "react-native";
+import {View, StatusBar, StyleSheet} from "react-native";
 import SlideBanner from "./Components/SlideBanner";
+import CategoryIcon from "./Components/CategoryIcon";
 
 export default class index extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <StatusBar barStyle={'light-content'}/>
+        <CategoryIcon />
         <SlideBanner />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+})
+
 
