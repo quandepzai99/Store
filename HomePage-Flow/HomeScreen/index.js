1faa48b51e6e0e37ce527234d4220186204e1cc1
Veiler <linhnv110196@gmail.com>
2020-06-09 15:36:46 +0700

    Login Flow Final Build linked Home
    Signed-off-by: Veiler <linhnv110196@gmail.com>

--- HomePage-Flow/HomeScreen/index.js
+++ HomePage-Flow/HomeScreen/index.js
@@ -1,27 +1,20 @@
 import React, { Component } from "react";
-import {
-  View,
-  StatusBar,
-  StyleSheet,
-  ScrollView,
-  SafeAreaView
-} from "react-native";
+import { StatusBar, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 import News from "./Components/News";
 import CategoryIcon from "./Components/CategoryIcon";
 import Header from "./Components/Header";
 import FavoriteBrands from "./Components/FavoriteBrands";
 
-
 export default function Class() {
   return (
     <ScrollView>
+      <StatusBar translucent={true} backgroundColor={"transparent"}/>
       <SafeAreaView style={styles.container}>
         <StatusBar />
         <Header />
         <CategoryIcon />
         <News />
         <FavoriteBrands />
-
       </SafeAreaView>
     </ScrollView>
   );
