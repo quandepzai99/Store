7a59934d857a88c46dca43bd7739f0213c0489dc
Veiler <linhnv110196@gmail.com>
2020-06-10 16:32:11 +0700

    fix style
    Signed-off-by: Veiler <linhnv110196@gmail.com>

--- HomePage-Flow/HomeScreen/Components/BottomTabView.js
+++ HomePage-Flow/HomeScreen/Components/BottomTabView.js
@@ -50,8 +50,7 @@ function Item({ item }) {
           height: 112
         }}
       />
-      <View
-        style={styles.iconStyle}>
+      <View style={styles.iconStyle}>
         <Image
           resizeMode="contain"
           source={item.image}
@@ -123,28 +122,29 @@ const styles = StyleSheet.create({
     },
     shadowRadius: 10,
     shadowOpacity: 1,
-    alignItems: "center"
+    alignItems: "center",
+    marginRight: 3
   },
   tabView: {
     flex: 1,
-    padding: 10,
+    marginLeft: 5,
     backgroundColor: "rgba(0,0,0,0.01)"
   },
-    iconStyle : {
-        width: 72,
-        height: 72,
-        borderRadius: 38,
-        backgroundColor: "white",
-        alignItems: "center",
-        justifyContent: "center",
-        top: -5,
-        shadowColor: "rgba(22, 60, 132, 0.1)",
-        shadowOffset: {
-            width: 0,
-            height: 3
-        },
-        shadowRadius: 10,
-        shadowOpacity: 1,
-        position: "absolute"
-    }
+  iconStyle: {
+    width: 72,
+    height: 72,
+    borderRadius: 38,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    top: -5,
+    shadowColor: "rgba(22, 60, 132, 0.1)",
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 10,
+    shadowOpacity: 1,
+    position: "absolute"
+  }
 });
